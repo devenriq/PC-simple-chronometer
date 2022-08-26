8b455e8759dc190c2fedb7a8ee92959a7eee701e
Enrique <octepc.97@gmail.com>
2022-08-26 18:13:15 -0500

    made some steps in order to finish the project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const secondsSpan = document.querySelector("#seconds");
 const minutesSpan = document.querySelector("#minutes");
+const timerButton = document.querySelector("#timer-button");
+const hero = document.querySelector("#principal");
 
 let secondsValue = 0;
 let minutesValue = 0;
 let currentChronometer;
+let currentButton;
 
 function startChronometer() {
+  currentButton = event.target;
+  currentButton.disabled = true;
   currentChronometer = setInterval(() => {
     // console.log("Ha pasado un segundo");
     secondsValue += 1;
@@ -25,6 +30,9 @@ function formatValue(value) {
 }
 
 function stopChronometer() {
+  if (currentButton) {
+    currentButton.disabled = false;
+  }
   clearInterval(currentChronometer);
 }
 
@@ -33,4 +41,37 @@ function resetChronometer() {
   secondsValue = 0;
   secondsSpan.textContent = "00";
   minutesSpan.textContent = "00";
+}
+
+function executeTimer() {
+  hero.innerHTML = `
+    <h1 class="hero--title">Chronometer</h1>
+    <div class="hero--time">
+      <p id="time">
+        <span id="minutes">00</span>:<span id="seconds">00</span>
+      </p>
+    </div>
+    <div class="hero-buttons">
+      <button
+        class="button hero--button"
+        type="button"
+        onclick="startChronometer()"
+      >
+        Start</button
+      ><button
+        class="button hero--button"
+        type="button"
+        onclick="stopChronometer()"
+      >
+        Stop
+      </button>
+      <button
+        class="button hero--button"
+        type="button"
+        onclick="resetChronometer()"
+      >
+        Reset
+      </button>
+    </div>
+  `;
 }
